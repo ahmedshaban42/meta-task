c3b4aea52e450607bf1be3964bacefff1c7dcb6f
Ahmed Shaban <ahmed530sh@gmail.com>
2026-01-30 14:57:56 +0200

    feat: file constants

---
+++ src/constants/constants.js
@@ -0,0 +1,29 @@
+
+
+
+
+export const roles={
+    USER:'user',
+    ADMIN:'admin'
+}
+
+
+export const gender={
+    MALE:'male',
+    FEMALE:'female'
+}
+
+
+export const taskStatus = {
+    PENDING: 'pending',
+    IN_PROGRESS: 'in progress',
+    COMPLETED: 'completed'
+};
+
+export const logActions = {
+    CREATE_TASK: 'CREATE_TASK',
+    UPDATE_TASK: 'UPDATE_TASK',
+    DELETE_TASK: 'DELETE_TASK',
+    USER_LOGIN: 'USER_LOGIN',
+    USER_LOGOUT: 'USER_LOGOUT',
+};
